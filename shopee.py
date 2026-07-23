844a06bf44ffefd1f3e89265b63028239235444f
Rafael Novais de Miranda <56369172+rafaelnm90@users.noreply.github.com>
2026-07-23 19:25:13 -0300

    Update shopee.py

--- shopee.py
+++ shopee.py
@@ -367,7 +367,8 @@ teclado_edicao_nicho = ReplyKeyboardMarkup(
 def obter_teclado_raiz():
     botoes = [
         [KeyboardButton(text="Canal Afiliados 📺"), KeyboardButton(text="Outros Canais 🗂️")],
-        [KeyboardButton(text="Relatório Geral 📊"), KeyboardButton(text="Opções do Servidor ⚙️")]
+        [KeyboardButton(text="Relatório Geral 📊"), 
+        [KeyboardButton(text="Opções do Servidor ⚙️")]
     ]
     return ReplyKeyboardMarkup(keyboard=botoes, resize_keyboard=True, is_persistent=True)
 
