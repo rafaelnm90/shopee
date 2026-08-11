b3ec030eb5e82b742525d03a86a7dc0bff5ce497
Rafael Novais de Miranda <56369172+rafaelnm90@users.noreply.github.com>
2026-08-11 14:37:02 -0300

    Update api_gemini.py

--- api_gemini.py
+++ api_gemini.py
@@ -20,8 +20,7 @@ MODELOS_CASCATA_GEMINI = [
     "gemini-3.1-flash-lite",
     "gemini-2.5-flash",
     "gemini-2.5-flash-lite",
-    "gemini-3.1-flash-lite-preview",
-    "gemini-2.5-pro"
+    "gemini-3.1-flash-lite-preview"
 ]
 
 logger = logging.getLogger("API_Gemini")
