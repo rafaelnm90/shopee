7dfd199281b0a20c9158622f32ce39e52fd8b75d
Rafael Novais de Miranda <56369172+rafaelnm90@users.noreply.github.com>
2026-08-31 02:33:07 -0300

    Update api_gemini.py

--- api_gemini.py
+++ api_gemini.py
@@ -15,19 +15,15 @@ client_genai = genai.Client(api_key=GEMINI_API_KEY)
 MODELOS_CASCATA_GEMINI = [
     "gemini-3.1-pro-preview",
     "gemini-3.7-flash",
-    "gemini-3.7-flash-lite-preview",
-    "gemini-3.7-flash-lite",
     "gemini-3.6-flash",
     "gemini-3.6-flash-lite",
-    "gemini-3.6-flash-lite-preview"
+    "gemini-3.6-flash-lite-preview",
     "gemini-3.5-flash",
     "gemini-3.5-flash-lite",
-    "gemini-3.5-flash-lite-preview",
     "gemini-3-flash-preview",
     "gemini-3.1-flash-lite",
     "gemini-2.5-flash",
     "gemini-2.5-flash-lite",
-    "gemini-2.5-flash-lite-preview",
     "gemini-3.1-flash-lite-preview"
 ]
 
